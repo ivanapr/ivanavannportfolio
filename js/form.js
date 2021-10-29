a43dead4cd7579d923beb0c6b5820d1cd28e204a
Ivana Provvidenza <vanndere@gmail.com>
2021-10-29 05:23:34 -0300

    Fix form

--- js/form.js
+++ js/form.js
@@ -1,35 +1,74 @@
-function validar_envio() {
-    //valido el nombre
-    if (document.fvalida.nombre.value.length == 0) {
-        alert("Tiene que escribir su nombre")
-        document.fvalida.nombre.focus()
-        return 0;
-    }
-
-    //valido la edad. tiene que ser entero mayor que 18
-    edad = document.fvalida.edad.value
-    edad = validarEntero(edad)
-    document.fvalida.edad.value = edad
-    if (edad == "") {
-        alert("Tiene que introducir un número entero en su edad.")
-        document.fvalida.edad.focus()
-        return 0;
-    } else {
-        if (edad < 18) {
-            alert("Debe ser mayor de 18 años.")
-            document.fvalida.edad.focus()
-            return 0;
-        }
-    }
-
-    //valido el interés
-    if (document.fvalida.interes.selectedIndex == 0) {
-        alert("Debe seleccionar un motivo de su contacto.")
-        document.fvalida.interes.focus()
-        return 0;
-    }
-
-    //el formulario se envia
-    alert("Muchas gracias por enviar el formulario");
-    document.fvalida.submit();
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+const expresiones = {
+    fname: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras y espacios, pueden llevar tildes.
+    mail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    phone: /^\d{8,14}$/, // 8 a 14 numeros.
+    subject: /^[a-zA-ZÀ-ÿ\s]{1,40}$/
 }
+
+const campos = {
+    fname: false,
+    mail: false,
+    phone: false,
+    subject: false
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "fname":
+            validarCampo(expresiones.fname, e.target, 'fname');
+            break;
+        case "mail":
+            validarCampo(expresiones.mail, e.target, 'mail');
+            break;
+        case "phone":
+            validarCampo(expresiones.phone, e.target, 'phone');
+            break;
+        case "subject":
+            validarCampo(expresiones.subject, e.target, 'subject');
+            break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
+    } else {
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (campos.fname && campos.mail && campos.phone && campos.subject) {
+        alert("Muchas gracias por enviar el formulario");
+        formulario.reset();
+        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+        }, 5000);
+
+        document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+            icono.classList.remove('formulario__grupo-correcto');
+        });
+    } else {
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+    }
+});
